ef0bfe7ef5dc56d5cc65da555d15155d19921f94
Brendan Shelton <bpshelton01@gmail.com>
2017-10-06 14:35:00 -0500

    Added a basic form

---
+++ create-account.php
@@ -0,0 +1,30 @@
+<html>
+    <head>
+    </head>
+
+    <body>
+        <h1>Create Account</h1>
+        <form action="" method="POST">
+            <div>Account Type: 
+                <select id="accountType" name="accountType">
+                    <option>Administrator</option>
+                    <option>Teacher</option>
+                    <option>Student</option>
+                </select>
+            </div>
+
+            <div>Name: <input type="text" id="name" name="name">
+            </div>
+
+            <div>Username: <input type="text" id="username" name="username">
+            </div>
+
+            <div>Email: <input type="email" id="email" name="email">
+            </div>
+
+            <div><button id="submit" type="submit">Submit</button> </div>
+
+        </form>
+    </body>
+
+</html>
