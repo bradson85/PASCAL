381aed1f268194ee4e2a2cd26fd25a496ea1d6b7
Media <augustgate@Livestream.stjacob.hometel.com>
2017-10-07 22:36:40 -0500

    Funtional Table the can be dynamically added to and edited to save words and definitions to database. Still needs database

---
+++ js/Addwords.js
@@ -0,0 +1,68 @@
+$(document).ready(function(){
+    var rowCount;
+
+    // this adds a new row for adding more words
+       $('#addRow').click(function(event){
+        event.preventDefault();
+        rowCount = $('#word_table tr').length; // because of the header row count is already plus 1
+        var wordnumber= $('#word_table tr:last-child td:first-child').html();
+        var id = parseInt(wordnumber)
+        var newRow = $('<tr><td contenteditable= "true" id = "cell'+ (id+1) +'_'+ '1'
+          + '">'+ (id+1) +'</td><td contenteditable= "true" id = "cell'+ (id+1) +'_'+ '2'
+          + '">new</td><td contenteditable= "true" id = "cell'+ (id+1) +'_'+ '3'
+          + '">input</td><td contenteditable= "true" id = "cell'+ (id+1) +'_'+ '4'
+          + '">new</td><td contenteditable= "true" id = "cell'+ (id+1) +'_'+ '5'
+          + '">2</td><td id = "cell'+ (id+1) +'_'+ '6'
+          + '"><button  class="deleteRow" id= "deleteRow'+ (id+1)+'">Delete</button></td?</tr>');
+        $('#word_table').append(newRow);
+        });
+        
+        
+         
+        $(document).on("click",".deleteRow" ,function(){
+           $( $(this).parents('tr')).remove();
+            var wordnumber= $('#word_table tr:last-child td:first-child').html();
+            var totalnumber = parseInt(wordnumber)
+        });
+
+        $('[contenteditable="true"]').focus(function(){
+            $(this).on('keydown', function(e) {
+                if (e.which == 13 && e.shiftKey == false) {
+                    $.post( 
+                        "save.php",
+                        { id: "Zara",
+                        category: "",
+                        word: "",
+                        definition: "",
+                        difficulty: "",
+                        save: "true"
+                      },
+                        function(data) {
+                           //do nothing
+                        }
+                     );
+                   $ (this).blur();
+                  return false;
+                }else if(e.which == 27){
+                    $.post( 
+                        "save.php",
+                        { id: "Zara",
+                          category: "",
+                          word: "",
+                          definition: "",
+                          difficulty: "",
+                          save: "false"
+                        },
+                        function(data) {
+                           $(this).html(data);
+                        }
+                     );
+                }
+              });
+        });
+
+        $('[contenteditable="true"]').blur(function(){
+            // save using ajax
+        });
+
+});
