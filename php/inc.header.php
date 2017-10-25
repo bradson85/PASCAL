2969d240400bdee2d6afeb6608184a453fd58843
Brendan Shelton <brshelt@siue.edu>
2017-10-25 11:51:44 -0500

    Started header file

---
+++ php/inc.header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+        <meta charest="utf-8" name="viewport" content="width=device-width, initial-scale=1.0">
+        <title><?php echo $page_title; ?></title>
+        <link href="<?php echo $css_path; ?>" rel="stylesheet">
+    </head>
