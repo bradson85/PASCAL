5af58fb38214904f530fc4777fa8c08d4180bf9b
Brendan Shelton <brshelt@siue.edu>
2017-10-25 21:17:45 -0500

    This commit refs #708 @0.5h Additional work to parse the ID from the URL.

--- create-password.php
+++ create-password.php
@@ -4,6 +4,11 @@
 
     include('php/inc.header.php');
     include('php/inc.create-password.php');
+    // Get the ID from the URL and pass it to the getUser function to be processed and find the userID
+    $id = $_GET['id'];
+    $user = getUser($id);
+
+    echo $user;
 ?>
 
     <body>
@@ -13,11 +18,11 @@
             <form action="php/inc.create-password.php" method="POST" id="createPassword">
                 <div class="form-group">
                     <label for="password">Password:</label>
-                    <input type="password" class="form-control" id="password" placeholder="Password">
+                    <input type="password" class="form-control" id="password" name="password" placeholder="Password">
                 </div>
                 <div class="form-group">
                     <label for="confirm">Confirm Password:</label>
-                    <input type="password" class="form-control" id="confirm" placeholder="Confirm Password">
+                    <input type="password" class="form-control" id="confirm" name="confirm" placeholder="Confirm Password">
                 </div>
     
                 <button id="submit" type="submit" class="btn btn-primary">Submit</button>
