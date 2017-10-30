04d4b8cfec0124dadbee040007617e3af3648f75
Brendan Shelton <brshelt@siue.edu>
2017-10-30 11:36:55 -0500

    This commit refs #672 @0.5h
    Testing drag and drop functionality.

---
+++ assessment.php
@@ -0,0 +1,25 @@
+<?php
+    $page_title = "Assessment";
+    $css_path = "css/main.css";
+
+    include ('php/inc.header.php');
+    include ('php/inc.assessment.php');
+?>
+
+    <body>
+        <div class="container">
+            <div class="card" id="test">
+                <div class="card-body">
+                    This is a test card
+                </div>
+            </div>
+        </div>
+
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+        <script src="js/jquery.ui.touch-punch.min.js"></script>
+        <script src="js/assessment.js"></script>
+    </body>
---
+++ php/inc.header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+        <meta charest="utf-8" name="viewport" content="width=device-width, initial-scale=1.0">
+        <title><?php echo $page_title; ?></title>
+        <link href="<?php echo $css_path; ?>" rel="stylesheet">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
+    </head>
