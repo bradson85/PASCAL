18ab42d8903f82e6d397574145fde09e66724afa
Brendan Shelton <brshelt@siue.edu>
2017-11-02 23:01:15 -0500

    This commit refs #674 @0.5h Continued work on integration with database.

--- php/inc.assessment.php
+++ php/inc.assessment.php
@@ -1,3 +1,21 @@
 <?php
-    
+    include ('inc.functions.php');
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST')
+    {
+        $assessID = $_POST['id'];
+        $studentID = $_POST['student'];
+
+        $pdo = pdo_construct();
+        $sql = "SELECT catID, classID FROM assessments WHERE assessment.ID = $id";
+        $result = pdo_query($pdo, $sql);
+
+        $pdo = null;
+        $pdo = pdo_construct();
+        $sql = "SELECT terms.name, terms.definition FROM terms, categories WHERE categories.name = '$catName' AND categories.ID = terms.catID";
+
+        // Execute the query, and return the tuples.
+        $data = pdo_query($pdo, $sql);
+        echo $data;
+    }
 ?>
