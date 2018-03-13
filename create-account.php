508f8c6df782a45feac1ee599f8c662e9c530299
Brendan Shelton <brshelt@siue.edu>
2018-03-13 12:23:10 -0500

    Fix test variables

--- create-account.php
+++ create-account.php
@@ -1,7 +1,7 @@
 <?php 
     session_start();
-    $_SESSION['user'] = "Name";
-    $_SESSION['email'] = "student@test.com";
+    //$_SESSION['user'] = "Name";
+    //$_SESSION['email'] = "student@test.com";
     $page_title = "Create Account";
     $css_path = "css/main.css";
 
